0847f2ac168cb3d56d6640b1cd8dc3f5396cd576
Carlos Umaña <dev.carlos.umana@gmail.com>
2024-05-09 10:25:48 -0500

    [feat]: setup initial styles

--- app/root.tsx
+++ app/root.tsx
@@ -1,5 +1,11 @@
 import { Links, Meta, Outlet, Scripts, ScrollRestoration } from "@remix-run/react";
 
+import type { LinksFunction } from "@remix-run/node";
+
+import globalStyles from "~/styles/global.css?url";
+
+export const links: LinksFunction = () => [{ rel: "stylesheet", href: globalStyles }];
+
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
