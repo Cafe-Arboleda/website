e390398c2a0cff1cb198c43cfd007ca1b0f1fe2d
Carlos Umaña <dev.carlos.umana@gmail.com>
2024-05-11 12:37:31 -0500

    [feat]: create recover password form

--- app/routes/_auth+/recover-password/route.tsx
+++ app/routes/_auth+/recover-password/route.tsx
@@ -1,7 +1,24 @@
+import { Form } from "@remix-run/react";
+
+import { Button } from "~/components/button";
+import { TextInput } from "~/components/text-input";
+import { SignFormWrapper } from "~/components/sign-form-wrapper";
+
+import styles from "./route.module.css";
+
 export default function RecoverPassswordRoute() {
   return (
-    <div>
-      <h1>Recover Password</h1>
-    </div>
+    <SignFormWrapper title="Hola de nuevo!">
+      <Form className={styles.form}>
+        <TextInput
+          label="Te enviaremos un correo con instrucciones para recuperar tu contraseña"
+          type="email"
+          name="email"
+          placeholder="Correo registrado"
+        />
+
+        <Button>Recuperar Contraseña</Button>
+      </Form>
+    </SignFormWrapper>
   );
 }
